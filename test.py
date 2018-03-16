a9d5af6f5e10a92e9e38137f3de8a0b76574c9e7
wy <814321439@qq.com>
2018-03-15 21:26:17 -0700

    test06

--- test.py
+++ test.py
@@ -12,7 +12,7 @@ print('num test')
 '''1-100质数'''
 def primenumber():
     print("test git~")
-    print("test000005")
+    print("test000006")
     for num in range(1,100):
         for i in range(2,num):
             if (num%i ==0):
